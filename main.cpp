ab673d545919049cd80f676e7a24573ab9cfc991
Saphirion <telikinez@gmail.com>
2022-10-13 14:03:31 +0300

    Add smoker thread function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <chrono>
 
 enum missing_component: int{
+	wait,
 	tobacco,
 	paper,
 	match
@@ -14,6 +16,19 @@ void increase_counter(int &c){
 	c++;
 }
 
+std::mutex smoke_iter_mutex;
+missing_component current_missing_component = wait;
+void smocker(int &c, int max_c, missing_component possession){
+	while(c < max_c){
+		if(current_missing_component == possession){
+			std::lock_guard<std::mutex> guard(smoke_iter_mutex);
+			current_missing_component = wait;
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+			std::cout << "Current iteration: " << c << std::endl;
+			increase_counter(c);
+		}
+	}
+}
 
 int main(){
 	return 0;
