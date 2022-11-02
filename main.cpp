5324a8834be317b43dab4ea2b581ab76bdd74cab
Saphirion <telikinez@gmail.com>
2022-11-02 13:53:36 +0300

    Implement main function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include "func.h"
+#include <mutex>
+#include <thread>
 
 int main(){
+	int counter = 0;
+	constexpr int max_c = 20;
+	std::mutex counterMutex;
+	std::mutex smokeIterMutex;
+	MissingComponent currentMissingComponent = wait;
+	int sequencer = 0;
+	std::thread s1(smocker, std::ref(counter), max_c, std::ref(sequencer), MissingComponent(paper), std::ref(currentMissingComponent), std::ref(smokeIterMutex));
+	std::thread s2(smocker, std::ref(counter), max_c, std::ref(sequencer), MissingComponent(tabacco), std::ref(currentMissingComponent), std::ref(smokeIterMutex));
+	std::thread s3(smocker, std::ref(counter), max_c, std::ref(sequencer), MissingComponent(match), std::ref(currentMissingComponent), std::ref(smokeIterMutex));
+	std::thread d(dealer, std::ref(counter), max_c, std::ref(sequencer), std::ref(currentMissingComponent), std::ref(counterMutex), std::ref(smokeIterMutex));
+	d.join();
+	s1.join();
+	s2.join();
+	s3.join();
 	return 0;
 }
