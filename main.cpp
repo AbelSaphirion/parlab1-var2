dae7adac5e8d4d503ba7b63468c31ebc15e8317e
Saphirion <saphirion@Bakery.localdomain>
2022-11-03 13:11:34 +0300

    Add cout to main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include "func.h"
 #include <mutex>
 #include <thread>
+#include <iostream>
 
 int main(){
 	int counter = 0;
-	constexpr int max_c = 5;
+	constexpr int max_c = 20;
 	std::mutex counterMutex;
 	std::mutex smokeIterMutex;
 	MissingComponent currentMissingComponent = Wait;
@@ -12,6 +13,10 @@ int main(){
 	std::thread s2(smocker, std::ref(counter), max_c, MissingComponent(Tabacco), std::ref(currentMissingComponent), std::ref(counterMutex), std::ref(smokeIterMutex));
 	std::thread s3(smocker, std::ref(counter), max_c, MissingComponent(Match), std::ref(currentMissingComponent), std::ref(counterMutex), std::ref(smokeIterMutex));
 	std::thread d(dealer, std::ref(counter), max_c, std::ref(currentMissingComponent), std::ref(smokeIterMutex));
+	while(counter < max_c){
+		std::cout << "Iter " << counter << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(2));
+	}
 	d.join();
 	s1.join();
 	s2.join();
