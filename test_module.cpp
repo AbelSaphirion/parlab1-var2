4ae5af07ba3498a3ba24d4167b854def36575303
matveykasalimov <90252106+matveykasalimov@users.noreply.github.com>
2022-11-03 13:58:53 +0300

    Init varuables

---
+++ test_module.cpp
@@ -0,0 +1,19 @@
+#include "func.h"
+#include <atomic>
+#include <mutex>
+#include <thread>
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Test
+#include <boost/test/included/unit_test.hpp>
+
+int c = 0;
+int c_max = 1;
+std::mutex counterMutex;
+std::mutex smokeIterMutex;
+MissingComponent currentMissingComponent = Wait;
+std::atomic<bool> done1(false);
+std::atomic<bool> done2(false);
+std::atomic<bool> done3(false);
+std::atomic<bool> done4(false);
+std::atomic<bool> done5(false);
+std::atomic<bool> done6(false);
